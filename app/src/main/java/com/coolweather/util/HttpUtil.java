4569e4f5f25fce4d61c8fca7265abf5442214035
itzzuli <519462367@qq.com>
2017-02-05 11:39:47 +0800

    添加fragment

---
+++ app/src/main/java/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by wxc on 2017/2/4.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
